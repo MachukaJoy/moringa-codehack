5d4a219a011e7c965331b23414cd872dd544562c
KakushaVenecia <kakushavenecia@gmail.com>
2022-07-10 02:03:51 +0300

    bg-Fixing bugs

--- frontend/moringa-codehack/src/app/components/tm-dashboard/tm-dashboard.component.ts
+++ frontend/moringa-codehack/src/app/components/tm-dashboard/tm-dashboard.component.ts
@@ -14,18 +14,12 @@ export class TmDashboardComponent implements OnInit {
   constructor(private katas:KatasService) { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  this.katas.get_katas().subscribe((kata)=> {
-    this.kataQuestions = kata;
-    console.log(this.kataQuestions)
-=======
-=======
-  this.katas.get_katas().subscribe((kata)=> {
-    this.kataQuestions = kata;
-    console.log(this.kataQuestions)
 
->>>>>>> dacf8c4a831890350c7ff13c65af457ae24c5db2
+
+  this.katas.get_katas().subscribe((kata)=> {
+    this.kataQuestions = kata;
+    console.log(this.kataQuestions)
+})
     // var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
     // var yValues = [55, 49, 44, 24, 15];
     // var barColors = [
@@ -52,17 +46,16 @@ export class TmDashboardComponent implements OnInit {
     //     }
     //   }
     // });
-<<<<<<< HEAD
->>>>>>> acf38a2e606815be1e802dd0216e377f32809a24
-=======
->>>>>>> dacf8c4a831890350c7ff13c65af457ae24c5db2
-  }
-    )
+
+
+
 
   }
  
-  sendQuestion(): void {
+sendQuestion(): void {
    this.katas.studentAssessment=this.createdAssessment
    console.log(this.katas.studentAssessment)
   }
 }
+
+
