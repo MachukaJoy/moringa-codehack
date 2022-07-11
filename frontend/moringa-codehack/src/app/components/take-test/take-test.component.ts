663b14b9f042325a10e73b1148b5bb063a81ce14
samuel <samuel.martins4.sm@gmail.com>
2022-07-11 20:53:27 +0300

    style: update changes to the code playground

--- frontend/moringa-codehack/src/app/components/take-test/take-test.component.ts
+++ frontend/moringa-codehack/src/app/components/take-test/take-test.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AnyForUntypedForms } from '@angular/forms';
 import { KatasService } from 'src/app/katas.service';
-
 
 @Component({
   selector: 'app-take-test',
   templateUrl: './take-test.component.html',
-  styleUrls: ['./take-test.component.css']
+  styleUrls: ['./take-test.component.css'],
 })
 export class TakeTestComponent implements OnInit {
-
-  constructor(private kata: KatasService) { }
+  code!: string;
+  output!: string;
+  simpleKata!: any;
+  constructor(private kata: KatasService) {}
 
   ngOnInit(): void {
-    console.log(this.kata.studentAssessment)
+    this.kata.get_katas().subscribe((response: any) => {
+      console.log(response[0]);
+      this.simpleKata = response[0];
+    });
   }
-
 }
