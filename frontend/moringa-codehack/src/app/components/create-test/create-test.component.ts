897649707f73e3c7d904d4b49c6cb8ef6a915fc1
Machuka Joy <machukajoy@gmail.com>
2022-07-13 10:15:21 +0300

    st: updates styling of form

--- frontend/moringa-codehack/src/app/components/create-test/create-test.component.ts
+++ frontend/moringa-codehack/src/app/components/create-test/create-test.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { QuestionsService } from 'src/app/services/questions/questions.service';
-// import { IDropdownSettings, } from 'ng-multiselect-dropdown';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 
 
 @Component({
@@ -13,12 +14,19 @@ export class CreateTestComponent implements OnInit {
   katasList:any =[];
   multipleChoiceList:any = [];
   assessmentList:any = [];
+  @ViewChild("assessmentcategory") category:any;
+  
  
 
-  constructor(private subjective:QuestionsService, private katas:QuestionsService, private multiple:QuestionsService, private assessments:QuestionsService) { }
+  constructor(private subjective:QuestionsService, private katas:QuestionsService, private multiple:QuestionsService, private assessments:QuestionsService,private http: HttpClient) { }
+
+  response!: any;
+  message!: string;
+
+    
 
   ngOnInit(): void {
-    
+    console.log(this.category)
     let sendInvite = document.querySelector("#sendinvite") as HTMLDivElement;
     let inviteCheckbox = document.querySelector("#sendinvitecheckbox") as HTMLInputElement
     // let assesmentCheckbox = document.querySelector("#createassesmentcheckbox") as HTMLInputElement
@@ -46,25 +54,33 @@ export class CreateTestComponent implements OnInit {
     //   }
     // });
 
-    katascheck.addEventListener('change', function(){
+    katascheck.addEventListener('click', function(){
+      // console.log(this.checked)
       if (this.checked){
         katasselect.style.display='block'
+        // console.log(this.value)
+        multipleselect.style.display='none'
+        subjectiveselect.style.display='none'
       } else{
         katasselect.style.display='none'
       }
     });
 
-    multiplecheck.addEventListener('change', function(){
+    multiplecheck.addEventListener('click', function(e){
       if (this.checked){
         multipleselect.style.display='block'
+        katasselect.style.display='none'
+        subjectiveselect.style.display='none'
       } else{
         multipleselect.style.display='none'
       }
     });
 
-    subjectivecheck.addEventListener('change', function(){
+    subjectivecheck.addEventListener('click', function(e){
       if (this.checked){
         subjectiveselect.style.display='block'
+        katasselect.style.display='none'
+        multipleselect.style.display='none'
       } else{
         subjectiveselect.style.display='none'
       }
@@ -76,6 +92,8 @@ export class CreateTestComponent implements OnInit {
     this.getMultipleChoice()
     this.getAssessments()
   }
+
+
   getSubjective(){
     this.subjective.get_subjective().subscribe(response =>{
       console.log(response)
@@ -102,6 +120,30 @@ export class CreateTestComponent implements OnInit {
       console.log(response)
       this.assessmentList=response
     })
+  }
+
+  submit(assessmentName: string, assessmentTopic: string, assessmentDifficulty: string, assessmentPassmark: string,
+    assessmentTimelimit: string, assessmentCategory:any): void{
+      let assesmentform = document.querySelector("#createownassesment") as HTMLFormElement
+      console.log(assesmentform)
+
+      this.http
+      .post<object>('https://api-moringa-codehack.herokuapp.com/api/assessments/', {
+        name: assessmentName,
+        topic: assessmentTopic,
+        difficulty: assessmentDifficulty,
+        pass_mark: assessmentPassmark,
+        time_limit: assessmentTimelimit,
+        category: assessmentCategory.value
+        
+      })
+      .subscribe((res) => {
+        console.log(res);
+        this.response = res;
+        this.message = this.response.message;
+        
+      });
+      
   }
 
 }
