49f348deab68b215284a1e66fbe8f32511ea2491
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-14 01:40:32 +0300

    chore: hide API endpoint in the environment

--- frontend/moringa-codehack/src/app/services/authenticated-user/authenticated-user.service.ts
+++ frontend/moringa-codehack/src/app/services/authenticated-user/authenticated-user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/classes/user/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -10,20 +11,20 @@ export class AuthenticatedUserService {
   ngOnInit() {}
   getUser() {
     return this.http.get<User>(
-      'http://localhost:8000/api/authenticated_user/',
+      `${environment}/authenticated_user/`,
 
       { withCredentials: true }
     );
   }
 
   getStudentData(username: string) {
-    return this.http.post('http://localhost:8000/api/student_data/', {
+    return this.http.post(`${environment}/student_data/`, {
       username: username,
     });
   }
 
   logOut() {
-    return this.http.get('http://localhost:8000/api/logout/', {
+    return this.http.get(`${environment}/login/`, {
       withCredentials: true,
     });
   }
