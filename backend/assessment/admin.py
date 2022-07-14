15a17cf16c379876d847f7a7f2df2f7c53c030a7
samuel martins <samuel.martins4.sm@gmail.com>
2022-07-14 09:43:17 +0300

    Auto-generated commit [ci skip]

--- backend/assessment/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Assessment)
-admin.site.register(Grade)
-admin.site.register(Invite)
-admin.site.register(Feedback)
-
--- frontend/moringa-codehack/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiBase: 'https://api-moringa-codehack.herokuapp.com/api',
-  // apiBase: 'http://localhost:8000/api',
-};
